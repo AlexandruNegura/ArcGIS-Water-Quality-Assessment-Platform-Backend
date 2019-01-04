238721fe8637985d8ecdbbe13305d21601095167
AlexandruNegura <alexdn13@gmail.com>
2019-01-04 17:44:14 +0200

    Project setup

---
+++ src/main/java/ro/arcgis/mures/mures/MuresApplication.java
@@ -0,0 +1,14 @@
+package ro.arcgis.mures.mures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuresApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/ro/arcgis/mures/mures/MuresApplicationTests.java
@@ -0,0 +1,17 @@
+package ro.arcgis.mures.mures;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MuresApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
